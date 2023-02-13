7366ed1dd51a9d39b45ae1e4c0c520167eedbc60
Muayad <muayadhaddad@yahoo.com>
2023-02-14 00:09:25 +0300

    search screen

--- Netflix Clone/Controllers/Core/SearchViewController.swift
+++ Netflix Clone/Controllers/Core/SearchViewController.swift
@@ -16,10 +16,19 @@ class SearchViewController: UIViewController {
         return table
     }()
     
+    public let searchController : UISearchController = {
+        let controller = UISearchController(searchResultsController: SearchResultViewController())
+        controller.searchBar.placeholder = "Search for a Movie or a Tv show "
+        controller.searchBar.searchBarStyle = .minimal
+        return controller
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemBackground
+        // Title of navigation bar
         title = "Search"
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationItem.largeTitleDisplayMode = .always
@@ -39,7 +48,11 @@ extension SearchViewController{
         view.addSubview(discoverTableView)
         discoverTableView.delegate = self
         discoverTableView.dataSource = self
+        navigationItem.searchController  = searchController
+        // To change color of cancel Button to white
+        navigationController?.navigationBar.tintColor = .white
         fetcDiscoverMovies()
+        searchController.searchResultsUpdater = self
     }
     
     /// Fetch Discover Movie
@@ -61,7 +74,7 @@ extension SearchViewController{
 
 extension SearchViewController: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+        return titles.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -74,4 +87,27 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 140
     }
+}
+
+extension SearchViewController: UISearchResultsUpdating{
+    func updateSearchResults(for searchController: UISearchController) {
+        let searchBar = searchController.searchBar
+        
+        guard let query = searchBar.text, !query.trimmingCharacters(in: .whitespaces).isEmpty,query.trimmingCharacters(in: .whitespaces).count >= 3,
+              let resultsController = searchController.searchResultsController as? SearchResultViewController else{return}
+        
+        APICaller.shared.search(with: query) { results in
+            switch results{
+            case .success(let titles):
+                resultsController.titles = titles
+                DispatchQueue.main.async {
+                    resultsController.searchResultCollectionView.reloadData()
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    
 }
---
+++ Netflix Clone/Controllers/General/SearchResultViewController.swift
@@ -0,0 +1,59 @@
+//
+//  SearchResultViewController.swift
+//  Netflix Clone
+//
+//  Created by Muayad El-haddad on 13/02/2023.
+//
+
+// MARK: all of that form EPOSPDE 10
+import UIKit
+
+class SearchResultViewController: UIViewController {
+
+    public var titles : [Title] = [Title]()
+    
+    public  let searchResultCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: UIScreen.main.bounds.width / 3 - 10 , height: 200)
+        layout.minimumInteritemSpacing = 0
+        
+        let collectionView = UICollectionView(frame: .zero , collectionViewLayout: layout)
+        collectionView.register(TitleCollectionViewCell.self, forCellWithReuseIdentifier: TitleCollectionViewCell.reuseID)
+        return collectionView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+        setup()
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        searchResultCollectionView.frame = view.bounds
+    }
+}
+
+extension SearchResultViewController{
+    private func setup(){
+        searchResultCollectionView.delegate = self
+        searchResultCollectionView.dataSource = self
+        
+        view.addSubview(searchResultCollectionView)
+    }
+}
+
+extension SearchResultViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return titles.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TitleCollectionViewCell.reuseID, for: indexPath) as? TitleCollectionViewCell else{return UICollectionViewCell()}
+        
+        cell.configure(with: titles[indexPath.row].poster_path ?? "/xHlb7Vb7v50zWKLi8VihhIOHQEr.jpg")
+        return cell
+    }
+    
+    
+}
